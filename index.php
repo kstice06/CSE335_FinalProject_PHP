e7d6aa8eb2f7ef8e9c9e90049a48cc6f59724a39
Keirstin Stice <kastic02@louisville.edu>
2025-11-20 19:14:07 -0500

    Add basic structure for Classic Books website

--- index.php
+++ index.php
@@ -1,1 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Classic Books</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+
+<h1>Classic Books Website</h1>
+
+<div class="menu-container">
+    <label for="action">Choose an option:</label>
+    <select id="action" onchange="goToPage()">
+        <option value="">-- Select --</option>
+        <option value="randomize.php">Randomize by Genre</option>
+        <option value="browse.php">Browse</option>
+    </select>
+</div>
+
+<script>
+function goToPage() {
+    let page = document.getElementById("action").value;
+    if (page !== "") {
+        window.location.href = page;
+    }
+}
+</script>
+
+</body>
+</html>
 
