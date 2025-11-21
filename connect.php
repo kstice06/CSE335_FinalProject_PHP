9eb89b6d0aa36f9b3c1c54923c67fc889613b3fb
Keirstin Stice <kastic02@louisville.edu>
2025-11-20 19:13:22 -0500

    Add database connection setup in connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,13 @@
+<?php
+$host = "sql105.infinityfree.com";  
+$user = "if0_40469942";             
+$pass = "group3Classics";     
+$db   = "if0_40469942_cse335_classicbooks";
+
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
